e4cfd4429a8d2c6168dff9734701e401961732f8
karlaarenaata <202403307391@alunos.estacio.br>
2024-10-15 19:12:01 -0300

    update portfolio

---
+++ scripts.js
@@ -0,0 +1,18 @@
+function scrollToSection(sectionId) {
+    document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
+}
+function clickMenu() {
+    if (items.style.display == 'block') {
+        items.style.display = 'none'
+    } else {
+        items.style.display = 'block'
+    }
+}
+
+function mudouTamanho() {
+    if (window.innerWidth >=768) {
+        items.style.display = 'block'
+    } else {
+        items.style.display = 'none'
+    }
+}
